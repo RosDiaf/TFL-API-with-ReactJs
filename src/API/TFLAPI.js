07e9bb995c6608cd3b27442929c2eddf00748f60
RosDiaf <ros_diaf@hotmail.com>
2019-06-23 11:00:37 +0100

    constants file added

--- src/API/TFLAPI.js
+++ src/API/TFLAPI.js
@@ -1,16 +1,8 @@
-const APP_ID = 'a0e43da7';
-const APP_KEY = '237aa67a35d205346c515ace5c5578b1';
+import { constants } from './constants';
 
 export const getBikePointData = async () => {
-    const results = await fetch('https://api.tfl.gov.uk/BikePoint?app_id=' + APP_ID + '&app_key=' + APP_KEY)
+    const results = await fetch('https://api.tfl.gov.uk/BikePoint?app_id=' + constants.APP_ID + '&app_key=' + constants.APP_KEY)
     let response = await results.json();
     let myJson = await response;
     return myJson;
 };
-
-/*
-https://api.tfl.gov.uk/BikePoint?app_id=a0e43da7&app_key=237aa67a35d205346c515ace5c5578b1
-
-https://api.exchangeratesapi.io/latest
-
-*/
---
+++ src/API/constants.js
@@ -0,0 +1,4 @@
+export const constants  = {
+    APP_ID: 'a0e43da7',
+    APP_KEY: '237aa67a35d205346c515ace5c5578b1'
+}
